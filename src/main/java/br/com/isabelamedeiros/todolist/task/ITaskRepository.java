80ec1cca6d04f54cb973feaf468302078dc8f080
isabelamedeiros <isabela.medeiros@ibm.com>
2023-10-15 19:45:09 -0300

    criaçao de exceçoes e mensagens de erros

--- src/main/java/br/com/isabelamedeiros/todolist/task/ITaskRepository.java
+++ src/main/java/br/com/isabelamedeiros/todolist/task/ITaskRepository.java
@@ -2,8 +2,13 @@ package br.com.isabelamedeiros.todolist.task;
 
 import java.util.UUID;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
+
+    List<TaskModel> findByIdUser(UUID idUser);
+    TaskModel findByIdAndIdUser(UUID id, UUID idUser);
+
     
 }
